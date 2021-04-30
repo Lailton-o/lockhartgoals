c98dee02caeda8db8b582b139a7aae910f20dfab
Lailton <lai_kefca@hotmail.com>
2021-04-29 23:48:41 -0300

    Primeiro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import "nes.css/css/nes.min.css";
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="nes-container with-title">
+            <span class="title">Principal - 0,00062% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+       </div>
+       <div class="nes-container with-title">
+            <span class="title">Meta 1/2 - 0.0124% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div> 
+       <div class="nes-container with-title">
+            <span class="title">Meta 1 - 0.0062% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>       
+        <div class="nes-container with-title">
+            <span class="title">Meta 2 - 0.0031% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 3 - 0.00206% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 4 - 0.00155% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 5 - 0.00124% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 6 - 0.001033% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 7 - 0.0008857% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 8 - 0.000775% - Data 29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
+        <div class="nes-container with-title">
+            <span class="title">Meta 9 - 0.000689% - Data29/04/2021</span> 
+            <progress class="nes-progress is-primary" value="0.00062" max="100"></progress>
+        </div>
     </div>
   );
 }
